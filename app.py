d50966d9ed101d2a00277622120be23ee94be932
Ritik <ritik123453@gmail.com>
2022-02-14 01:54:22 +0530

    Initial commit

--- app.py
+++ app.py
@@ -121,5 +121,6 @@ def submit():
     return render_template("ritik.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",8080))
+    app.run(host='0.0.0.0',port=port)
 
