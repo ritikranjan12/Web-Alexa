087536bead8c98b0c0787e7721dd5b23ebfaefbf
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2022-02-14 20:02:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def submit():
         main()
     return render_template("ritik.html")
 
-port = int(os.environ.get("PORT",5000))
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=port,debug=True)
+    app.run(debug=False,host='0.0.0.0')
 
